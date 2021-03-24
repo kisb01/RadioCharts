c4eb05ae85d05f30e85e50d4f2222b5831fb0f84
kisb01 <kisb01@gmail.com>
2021-03-24 16:14:37 +0100

    Complete Radio Chart

--- src/main/java/com/codecool/database/RadioCharts.java
+++ src/main/java/com/codecool/database/RadioCharts.java
@@ -1,6 +1,58 @@
 package com.codecool.database;
 
 
+import java.sql.*;
+
 public class RadioCharts {
-    //TODO: implement this class
+    private final String DB_URL;
+    private final String USERNAME;
+    private final String PASSWORD;
+    private final Connection conn;
+
+    public RadioCharts(String DB_URL, String USERNAME, String PASSWORD) {
+        this.DB_URL = DB_URL;
+        this.USERNAME = USERNAME;
+        this.PASSWORD = PASSWORD;
+        this.conn = getConnection();
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Database not reachable.");
+        }
+        return connection;
+    }
+
+    public String getMostPlayedSong() {
+        String query = "SELECT song, SUM(times_aired) as aired FROM music_broadcast GROUP BY artist ORDER BY aired DESC LIMIT 1";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getString("song");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
+
+    public String getMostActiveArtist() {
+        String query = "SELECT artist, COUNT(DISTINCT song) as songs FROM music_broadcast GROUP BY artist ORDER BY songs DESC LIMIT 1";
+        try {
+            PreparedStatement ps = conn.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getString("artist");
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return "";
+    }
 }
